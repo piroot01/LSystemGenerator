120ea06b48d58a104e08f4c6913e530bb9eccbb4
piroot01 <piroot01@gmail.com>
2023-09-17 21:46:22 +0200

    Added Parameter

---
+++ LSystemGenerator/include/LSystemGenerator/Grammar/Parameter.hpp
@@ -0,0 +1,18 @@
+#ifndef Grammar_Parameter_INCLUDED
+#define Grammar_Parameter_INCLUDED
+
+
+#include <LSystemGenerator/Foundation/Foundation.hpp>
+
+
+namespace ls
+{
+
+
+using Parameter = char;
+
+
+} // namespace ls
+
+
+#endif

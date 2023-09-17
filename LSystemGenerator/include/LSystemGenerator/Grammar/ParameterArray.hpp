6f1be4552f95c6bdd9f4b10402b345e48daed283
piroot01 <piroot01@gmail.com>
2023-09-17 21:49:14 +0200

    Added ParameteterArray

---
+++ LSystemGenerator/include/LSystemGenerator/Grammar/ParameterArray.hpp
@@ -0,0 +1,20 @@
+#ifndef Grammar_ParameterArray_INCLUDED
+#define Grammar_ParameterArray_INCLUDED
+
+
+#include <LSystemGenerator/Foundation/Foundation.hpp>
+#include <LSystemGenerator/Grammar/Parameter.hpp>
+#include <vector>
+
+
+namespace ls
+{
+
+
+using ParameterArray = std::vector<Parameter>;
+
+
+} // namespace ls
+
+
+#endif

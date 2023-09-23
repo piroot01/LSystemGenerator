79b644952c485ba3692ecf96892a23edb6583bc8
piroot01 <piroot01@gmail.com>
2023-09-23 18:32:25 +0200

    Edited ParametrizedPredecessor test

--- test/Grammar/ParametrizedPredecessor.test.cpp
+++ test/Grammar/ParametrizedPredecessor.test.cpp
@@ -7,63 +7,77 @@ TEST_CASE("[Grammar] ls::ParametrizedPredecessor")
 {
     SECTION("Type traits")
     {
-        STATIC_CHECK_FALSE(std::is_copy_constructible_v<ls::ParametrizedPredecessor>);
-        STATIC_CHECK_FALSE(std::is_copy_assignable_v<ls::ParametrizedPredecessor>);
-        STATIC_CHECK_FALSE(std::is_nothrow_move_constructible_v<ls::ParametrizedPredecessor>);
-        STATIC_CHECK_FALSE(std::is_nothrow_move_assignable_v<ls::ParametrizedPredecessor>);
+        STATIC_CHECK(std::is_copy_constructible_v<ls::ParametrizedPredecessor_int>);
+        STATIC_CHECK(std::is_copy_assignable_v<ls::ParametrizedPredecessor_int>);
+        STATIC_CHECK(std::is_nothrow_move_constructible_v<ls::ParametrizedPredecessor_int>);
+        STATIC_CHECK(std::is_nothrow_move_assignable_v<ls::ParametrizedPredecessor_int>);
     }
 
     SECTION("Construction")
     {
         SECTION("Default constructor")
         {
-            STATIC_CHECK(std::is_default_constructible_v<ls::ParametrizedPredecessor>);
-            const ls::ParametrizedPredecessor parametrizedPredecessor;
+            STATIC_CHECK(std::is_default_constructible_v<ls::ParametrizedPredecessor_int>);
+            const ls::ParametrizedPredecessor_int parametrizedPredecessor;
             CHECK(parametrizedPredecessor.getParameterCount() == 0);
         }
 
         SECTION("Letter construction")
         {
-            const ls::ParametrizedPredecessor parametrizedPredecessor('A');
+            const ls::ParametrizedPredecessor_int parametrizedPredecessor('A');
             CHECK(parametrizedPredecessor.letter == 'A');
         }
     }
 
-    SECTION("operator[]")
+    SECTION("getParameterCount()")
     {
-        ls::ParametrizedPredecessor parametrizedPredecessor;
+        ls::ParametrizedPredecessor_int parametrizedPredecessor;
         for (std::size_t i = 0; i < 26; ++i)
         {
-            parametrizedPredecessor.append(static_cast<char>(65 + i));
-            CHECK(parametrizedPredecessor[i] == static_cast<char>(65 + i));
+            parametrizedPredecessor.insertParameter(static_cast<char>(65 + i), i);
         }
-        CHECK(parametrizedPredecessor[25] == 'Z');
+        CHECK(parametrizedPredecessor.getParameterCount() == 26);
+    }
+
+    SECTION("[]operator")
+    {
+        ls::ParametrizedPredecessor_int parametrizedPredecessor;
+        for (std::size_t i = 0; i < 26; ++i)
+        {
+            CHECK(parametrizedPredecessor.insertParameter(static_cast<char>(65 + i), i));
+            CHECK(std::size_t(parametrizedPredecessor[static_cast<char>(65 + i)]) == i);
+        }
+        CHECK_FALSE(parametrizedPredecessor.insertParameter('A', 10));
+        CHECK(parametrizedPredecessor['Z'] == 25);
     }
 
     SECTION("clear()")
     {
-        ls::ParametrizedPredecessor parametrizedPredecessor;
+        ls::ParametrizedPredecessor_int parametrizedPredecessor;
         for (std::size_t i = 0; i < 26; ++i)
-            parametrizedPredecessor.append(static_cast<char>(65 + i));
+            parametrizedPredecessor.insertParameter(static_cast<char>(65 + i), i);
         parametrizedPredecessor.clear();
         CHECK(parametrizedPredecessor.getParameterCount() == 0);
     }
 
-    SECTION("resize()")
+    SECTION("insertOrAssignParameter()")
     {
-        ls::ParametrizedPredecessor parametrizedPredecessor;
-        parametrizedPredecessor.resize(26);
-        for (std::size_t i = 0; i < parametrizedPredecessor.getParameterCount(); ++i)
-            CHECK(parametrizedPredecessor[i] == ls::Parameter());
+        ls::ParametrizedPredecessor_int parametrizedPredecessor;
+        ls::Parameter p = 'A';
+        parametrizedPredecessor.insertOrAssignParameter(p, 0);
+        parametrizedPredecessor.insertOrAssignParameter(p, 1);
+        CHECK(parametrizedPredecessor.getParameterCount() == 1);
+        CHECK(parametrizedPredecessor[p] == 1);
     }
 
-    SECTION("append()")
+    SECTION("insertParameter()")
     {
-        ls::ParametrizedPredecessor parametrizedPredecessor;
+        ls::ParametrizedPredecessor_int parametrizedPredecessor;
         ls::Parameter p = 'A';
-        parametrizedPredecessor.append(p);
+        CHECK(parametrizedPredecessor.insertParameter(p, 0));
+        CHECK_FALSE(parametrizedPredecessor.insertParameter(p, 1));
         CHECK(parametrizedPredecessor.getParameterCount() == 1);
-        CHECK(parametrizedPredecessor[0] == p);
+        CHECK(parametrizedPredecessor[p] == 0);
     }
 
 }

3c0c2dd47447a5aa05060d7cc53597c8bc47fb03
piroot01 <piroot01@gmail.com>
2023-09-23 18:26:01 +0200

    Allowed copy constructors

--- LSystemGenerator/include/LSystemGenerator/Grammar/Predecessor.hpp
+++ LSystemGenerator/include/LSystemGenerator/Grammar/Predecessor.hpp
@@ -12,27 +12,32 @@ namespace ls
 class LSystemAPI Predecessor
 {
 public:
-    constexpr Predecessor();
+    constexpr Predecessor() noexcept;
 
-    constexpr Predecessor(const char letter);
+    constexpr Predecessor(const Predecessor&) noexcept;
 
-    Predecessor(const Predecessor&) = delete;
+    constexpr Predecessor(Predecessor&&) noexcept;
 
-    Predecessor& operator=(const Predecessor&) = delete;
+    constexpr Predecessor& operator=(const Predecessor&) noexcept;
 
-    // For now it will be visible
-    char letter{};
+    constexpr Predecessor& operator=(Predecessor&&) noexcept;
 
 };
 
 
-constexpr inline Predecessor::Predecessor() = default;
+constexpr inline Predecessor::Predecessor() noexcept = default;
 
 
-constexpr inline Predecessor::Predecessor(const char letter) :
-    letter(letter)
-{
-}
+constexpr inline Predecessor::Predecessor(const Predecessor&) noexcept = default;
+
+
+constexpr inline Predecessor::Predecessor(Predecessor&&) noexcept = default;
+
+
+constexpr inline Predecessor& Predecessor::operator=(const Predecessor&) noexcept = default;
+
+
+constexpr inline Predecessor& Predecessor::operator=(Predecessor&&) noexcept = default;
 
 
 } // namespace ls

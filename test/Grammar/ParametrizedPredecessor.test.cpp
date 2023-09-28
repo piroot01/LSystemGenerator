64a41b738dc4d4ecf46da26109ed2ca994c916ed
piroot01 <piroot01@gmail.com>
2023-09-28 16:34:24 +0200

    Edited to support new ParameterizedPredecessor

--- test/Grammar/ParametrizedPredecessor.test.cpp
+++ test/Grammar/ParametrizedPredecessor.test.cpp
@@ -5,79 +5,55 @@
 
 TEST_CASE("[Grammar] ls::ParametrizedPredecessor")
 {
+    typedef double TestType;
+
     SECTION("Type traits")
     {
-        STATIC_CHECK(std::is_copy_constructible_v<ls::ParametrizedPredecessor_int>);
-        STATIC_CHECK(std::is_copy_assignable_v<ls::ParametrizedPredecessor_int>);
-        STATIC_CHECK(std::is_nothrow_move_constructible_v<ls::ParametrizedPredecessor_int>);
-        STATIC_CHECK(std::is_nothrow_move_assignable_v<ls::ParametrizedPredecessor_int>);
+        STATIC_CHECK(std::is_copy_constructible_v<ls::ParametrizedPredecessor<TestType>>);
+        STATIC_CHECK(std::is_copy_assignable_v<ls::ParametrizedPredecessor<TestType>>);
+        STATIC_CHECK(std::is_nothrow_move_constructible_v<ls::ParametrizedPredecessor<TestType>>);
+        STATIC_CHECK(std::is_nothrow_move_assignable_v<ls::ParametrizedPredecessor<TestType>>);
     }
 
     SECTION("Construction")
     {
-        SECTION("Default constructor")
+        SECTION("Default construction")
         {
-            STATIC_CHECK(std::is_default_constructible_v<ls::ParametrizedPredecessor_int>);
-            const ls::ParametrizedPredecessor_int parametrizedPredecessor;
-            CHECK(parametrizedPredecessor.getParameterCount() == 0);
+            STATIC_CHECK(std::is_default_constructible_v<ls::ParametrizedPredecessor<TestType>>);
         }
 
         SECTION("Letter construction")
         {
-            const ls::ParametrizedPredecessor_int parametrizedPredecessor('A');
-            CHECK(parametrizedPredecessor.letter == 'A');
+            ls::ParametrizedPredecessor<TestType> pararametrizedPredecessor('A');
+            CHECK(pararametrizedPredecessor.letter == 'A');
         }
+
     }
 
-    SECTION("getParameterCount()")
+    SECTION("Parameter acces")
     {
-        ls::ParametrizedPredecessor_int parametrizedPredecessor;
-        for (std::size_t i = 0; i < 26; ++i)
-        {
-            parametrizedPredecessor.insertParameter(static_cast<char>(65 + i), i);
-        }
-        CHECK(parametrizedPredecessor.getParameterCount() == 26);
+        ls::ParametrizedPredecessor<TestType> pararametrizedPredecessor('A');
+        pararametrizedPredecessor.parameters.insertOrAssignParameter('x', 1.2);
+        CHECK(pararametrizedPredecessor.parameters.insertParameter('y', 1.4));
+        CHECK(pararametrizedPredecessor.parameters.at('x') == 1.2);
+        CHECK(pararametrizedPredecessor.parameters.getParameterCount() == 2);
     }
 
-    SECTION("[]operator")
+    SECTION("addSymbols()")
     {
-        ls::ParametrizedPredecessor_int parametrizedPredecessor;
-        for (std::size_t i = 0; i < 26; ++i)
-        {
-            CHECK(parametrizedPredecessor.insertParameter(static_cast<char>(65 + i), i));
-            CHECK(std::size_t(parametrizedPredecessor[static_cast<char>(65 + i)]) == i);
-        }
-        CHECK_FALSE(parametrizedPredecessor.insertParameter('A', 10));
-        CHECK(parametrizedPredecessor['Z'] == 25);
-    }
-
-    SECTION("clear()")
-    {
-        ls::ParametrizedPredecessor_int parametrizedPredecessor;
-        for (std::size_t i = 0; i < 26; ++i)
-            parametrizedPredecessor.insertParameter(static_cast<char>(65 + i), i);
-        parametrizedPredecessor.clear();
-        CHECK(parametrizedPredecessor.getParameterCount() == 0);
-    }
-
-    SECTION("insertOrAssignParameter()")
-    {
-        ls::ParametrizedPredecessor_int parametrizedPredecessor;
-        ls::Parameter p = 'A';
-        parametrizedPredecessor.insertOrAssignParameter(p, 0);
-        parametrizedPredecessor.insertOrAssignParameter(p, 1);
-        CHECK(parametrizedPredecessor.getParameterCount() == 1);
-        CHECK(parametrizedPredecessor[p] == 1);
-    }
-
-    SECTION("insertParameter()")
-    {
-        ls::ParametrizedPredecessor_int parametrizedPredecessor;
-        ls::Parameter p = 'A';
-        CHECK(parametrizedPredecessor.insertParameter(p, 0));
-        CHECK_FALSE(parametrizedPredecessor.insertParameter(p, 1));
-        CHECK(parametrizedPredecessor.getParameterCount() == 1);
-        CHECK(parametrizedPredecessor[p] == 0);
+        ls::ParametrizedPredecessor<TestType> parametrizedPredecessor;
+        const std::string expressionString = "x + y < 3";
+        parametrizedPredecessor.parameters.insertOrAssignParameter('x', 1.2);
+        CHECK(parametrizedPredecessor.parameters.insertParameter('y', 1.4));
+        exprtk::symbol_table<TestType> symbolTable;
+        parametrizedPredecessor.registerSymbols(symbolTable);
+        exprtk::expression<TestType> expression;
+        expression.register_symbol_table(symbolTable);
+        exprtk::parser<TestType> parser;
+        parser.compile(expressionString, expression);
+        CHECK(expression.value());
+        parametrizedPredecessor.parameters['x'] = 1.99;
+        CHECK_FALSE(expression.value());
     }
 
 }
